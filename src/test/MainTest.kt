7ee5c385fa6e33f4fff236e6bee9cb09e0dcb466
Stanislav Klimovich <stasklimovish@gmail.com>
2026-05-22 23:56:22 +0300

    Update MainTest.kt

--- src/test/MainTest.kt
+++
@@ -1,6 +0,0 @@
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-}
-tasks.test {
-    useJUnitPlatform()
-}
---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+class MainTest {
+    @Test
+    fun testSomething() {
+        assertTrue(true)
+    }
+}
